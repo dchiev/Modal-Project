e7d2538e7ba830bfb31394b3d95c4c545aa200eb
Dan Chiev <danchiev2@Gmail.com>
2021-03-18 17:59:38 -0400

    added open and close events

--- script.js
+++ script.js
@@ -3,10 +3,21 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btn = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+const openModal = function () {
+  console.log('Button Clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button Clicked');
-    modal.classList.remove('hidden');
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
